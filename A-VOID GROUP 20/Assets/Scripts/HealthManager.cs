433527a9e4aa3006aa505f697aa0b176b73ef49d
ky13M <222689964+Ky13M15@users.noreply.github.com>
2025-10-20 17:46:35 +0200

    Revert "Main Menu Scene + Health manager +Audio sources"
    This reverts commit a37d364e935128e02eba7a0ba66fc91be5312d60.

--- A-VOID GROUP 20/Assets/Scripts/HealthManager.cs
+++ A-VOID GROUP 20/Assets/Scripts/HealthManager.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
+    public Image healthBar;
+    public float healthAmount = 100f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,8 +12,32 @@ public class HealthManager : MonoBehaviour
     }
 
     // Update is called once per frame
+    [System.Obsolete]
     void Update()
     {
-        
+        if(healthAmount <= 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            TakeDamage(20);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Heal(5);
+        }
+    }
+    public void TakeDamage(float damage)
+    {
+        healthAmount -= damage;
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+    public void Heal(float healingAmount)
+    {
+        healthAmount -= healingAmount;
+        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+
+        healthBar.fillAmount -= healthAmount /100f;
     }
 }
