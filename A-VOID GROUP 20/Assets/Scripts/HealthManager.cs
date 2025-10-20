317a38af8816098aa8f941c53ccc8d83341f2897
ky13M <222689964+Ky13M15@users.noreply.github.com>
2025-10-20 16:53:26 +0200

    Health Manager and UI event

--- A-VOID GROUP 20/Assets/Scripts/HealthManager.cs
+++ A-VOID GROUP 20/Assets/Scripts/HealthManager.cs
@@ -1,16 +1,86 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HealthManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Health Settings")]
+    public float maxHealth = 100f;
+    public float currentHealth;
+
+   
+    [Header("UI References")]
+    public Image healthBar; 
+    public GameObject DeathScreen;
+
+    [Header("Respawn Settings")]
+    public Transform spawnPoint;
+    public bool reloadSceneOnRespawn = false;
+
+
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        UpdateHealthUI();
+        if (DeathScreen != null) DeathScreen.SetActive(false);
+
+    }
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0,maxHealth);
+
+        UpdateHealthUI();
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+        currentHealth= Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthUI();
+    }
+    private void UpdateHealthUI()
+    {
+        if (healthBar != null)
+        {
+            healthBar.fillAmount = currentHealth / maxHealth;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+        // Update is called once per frame
+        private void Death()
     {
-        
+        GetComponent<FPController>().enabled = false;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        if (DeathScreen != null) DeathScreen.SetActive(true);
+        Debug.Log("GAME OVER!");
+
     }
+    public void ResetHealth()
+    {
+        currentHealth= maxHealth;
+        UpdateHealthUI();
+        if(DeathScreen != null) DeathScreen.SetActive(false);
+
+        if (reloadSceneOnRespawn)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else if (spawnPoint != null)
+        {
+            transform.position = spawnPoint.position; 
+            transform.rotation = spawnPoint.rotation; 
+        }
+        GetComponent<FPController>().enabled = true ;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
 }
